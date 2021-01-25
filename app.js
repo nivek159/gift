a6b2af8356ccc16b81815ce050face84e5ce99cf
nivek159 <67131545+nivek159@users.noreply.github.com>
2021-01-24 17:16:49 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
+// Service worker for Progressive Web App
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js')
-        .then(reg => console.log('service worker registered'))
-        .catch(err => console.log('service worker not registered', err));
+    navigator.serviceWorker.register('sw.js?v3', {
+        scope: '.' // THIS IS REQUIRED FOR RUNNING A PROGRESSIVE WEB APP FROM A NON_ROOT PATH
+    }).then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+    });
 }
