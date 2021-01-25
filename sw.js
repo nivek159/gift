2a11e30e9637a46ebccf1d7ce7cfc9ad0f95db15
nivek159 <67131545+nivek159@users.noreply.github.com>
2021-01-24 17:02:50 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,42 +1,41 @@
-var CACHE_NAME = 'pwgen-cache-v1';
-var urlsToCache = [
-    './?v3',
-    'sw.js?v3',
-    'manifest.json?v3',
-    'style.css?v3',
-    'script.js?v3',
-    'logo.png?v3',
-    'logo_192.png?v3',
-    'logo_256.png?v3',
-    'logo_512.png?v3',
-    'https://fonts.googleapis.com/css?family=Special+Elite'
-];
-console.log('loading sw');
+const staticCacheName = 'static-v1.0.0';
+const dynamicCacheName = 'dynamic-v1.0.0';
+const assets = [
+    '/'
+]
 
-self.addEventListener('install', function(event) {
-    // Perform install steps
-    console.log('installing sw');
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(function(cache) {
-                console.log('Opened cache');
-                var x = cache.addAll(urlsToCache);
-                console.log('cache added');
-                return x;
-            })
+// cache size limit function
+const limitCacheSize = (name, size) => {
+    caches.open(name).then(cache => {
+        cache.keys().then(keys => {
+            if (keys.length > size) {
+                cache.delete(keys[0]).then(limitCacheSize(name, size));
+            }
+        });
+    });
+};
+
+// install event
+self.addEventListener('install', evt => {
+    //console.log('service worker installed');
+    evt.waitUntil(
+        caches.open(staticCacheName).then((cache) => {
+            console.log('caching shell assets');
+            cache.addAll(assets);
+        })
     );
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request)
-            .then(function(response) {
-                    // Cache hit - return response
-                    if (response) {
-                        return response;
-                    }
-                    return fetch(event.request);
-                }
-            )
+// activate event
+self.addEventListener('activate', evt => {
+    //console.log('service worker activated');
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            //console.log(keys);
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName && key !== dynamicCacheName)
+                .map(key => caches.delete(key))
+            );
+        })
     );
 });
