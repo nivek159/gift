1bd55283d9bf911d5dfab422de7bd2fd6ad145c9
nivek159 <67131545+nivek159@users.noreply.github.com>
2021-01-24 17:06:35 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js?v2', {
-        scope: '.' // <--- THIS BIT IS REQUIRED
-    }).then(function(registration) {
-        // Registration was successful
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-        // registration failed :(
-        console.log('ServiceWorker registration failed: ', err);
-    });
+    navigator.serviceWorker.register('sw.js')
+        .then(reg => console.log('service worker registered'))
+        .catch(err => console.log('service worker not registered', err));
 }
